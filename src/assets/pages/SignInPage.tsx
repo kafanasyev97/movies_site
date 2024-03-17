c034ce31d669301bbccb5cae142fb03304abd835
Kirill <kirillmas222@gmail.com>
2024-03-17 21:37:46 +0400

    [upd] Sign in and sign up pages

---
+++ src/assets/pages/SignInPage.tsx
@@ -0,0 +1,12 @@
+import Form from '../components/RegistrationComponents/Form'
+
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>Login page</h1>
+      <Form buttonText="Войти" />
+    </div>
+  )
+}
+
+export default SignInPage
---
+++ src/assets/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import Form from '../components/RegistrationComponents/Form'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>Register page</h1>
+      <Form buttonText="Регистрация" />
+    </div>
+  )
+}
+
+export default SignUpPage
