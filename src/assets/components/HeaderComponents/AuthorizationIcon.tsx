24411db8fbfbd0a87f84e7cd191b8a8dfc5875ef
Kirill <kirillmas222@gmail.com>
2024-03-18 04:00:42 +0400

    [upd] Change auth icon

--- src/assets/components/HeaderComponents/AuthorizationIcon.tsx
+++ src/assets/components/HeaderComponents/AuthorizationIcon.tsx
@@ -1,9 +1,28 @@
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../userHooks/useAuth'
+import { useAppDispatch } from '../../../store/hooks'
+import { removeUser } from '../../../store/userSlice'
+
 const AuthorizationIcon = () => {
-  return (
-    <div>
-      <img className="authImg" src="/images/authorization.svg" alt="auth" />
-    </div>
-  )
+  const { isAuth } = useAuth()
+  const dispatch = useAppDispatch()
+
+  const showUserImg = () => {
+    if (isAuth) {
+      return (
+        <div onClick={() => dispatch(removeUser())}>
+          <img className="authImg" src="/images/logout.svg" alt="auth" />
+        </div>
+      )
+    }
+    return (
+      <Link to="/login">
+        <img className="authImg" src="/images/authorization.svg" alt="auth" />
+      </Link>
+    )
+  }
+
+  return <div className="header__auth">{showUserImg()}</div>
 }
 
 export default AuthorizationIcon
